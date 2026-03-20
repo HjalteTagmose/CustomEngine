4de813e571d00ceb664067ac797afec4c9d63100
Hjalte Tagmose <HjalteTagmose@users.noreply.github.com>
2026-03-20 20:20:37 +0100

    remove sdl, add sandbox

---
+++ src/Sandbox/Program.cs
@@ -0,0 +1,25 @@
+﻿using bottlenoselabs.SDL;
+
+var app = new SandboxApp();
+app.Run();
+
+class SandboxApp : Application
+{
+    private Window _window = null!;
+
+    protected override void OnStart()
+    {
+        _window = CreateWindow(new WindowOptions
+        {
+            Title = "Sandbox",
+            Width = 1280,
+            Height = 720
+        });
+    }
+
+    protected override void OnKeyDown(in KeyboardEvent e)
+    {
+        if (e.Button == KeyboardButton.Escape)
+            Exit();
+    }
+}
